071cf12c0732e5f844e40e9cafd87d80539bca28
Aritz Beraza <aritz.beraza@gmail.com>
2020-04-23 15:28:31 +0200

    Use .env file for secrets/credentials

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,8 @@
+import envVars from 'preact-cli-plugin-env-vars';
+import { lstatSync, readdirSync, existsSync } from 'fs';
 
+const isDirectory = source => lstatSync(source).isDirectory();
+const getDirectories = source => readdirSync(source).map( n => path.join(source, n)).filter(isDirectory);
 
 export default {
   plugins: [],
@@ -13,7 +17,8 @@ export default {
 	 * @param {object} options - this is mainly relevant for plugins (will always be empty in the config), default to an empty object
 	 **/
 	webpack(config, env, helpers, options) {
-    /** you can change the config here **/
-    console.log(config);
+		// Load .env file keys if available
+		envVars(config, env, helpers);
+
   },
 }
