6659d6a1d1f2da5928b5d7593ff36c9531397f08
Aritz Beraza <aritz.beraza@gmail.com>
2020-05-15 21:08:02 +0200

    eslint use spaces for indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest": true
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest": true
+  },
+  "extends": ["eslint:recommended", "standard-preact"],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": ["eslint:recommended", "standard-preact"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "rules": {
-        "quotes": ["off", "prefer-double", { "avoidEscape": true }],
-        "jsx-quotes": ["warn", "prefer-double"],
-        "react/jsx-indent":["warn", 'tab'],
-        "react/jsx-indent-props": ["warn", 'tab']
-    }
+    "sourceType": "module"
+  },
+  "rules": {
+    "quotes": ["off", "prefer-double", { "avoidEscape": true }],
+    "indent": ["warn", 2],
+    "jsx-quotes": ["warn", "prefer-double"],
+    "react/jsx-indent":["warn", 2],
+    "react/jsx-indent-props": ["warn", 'tab']
+  }
 };
