6c9442e53abc94c4a1de95df038bd45557a76a28
Aritz Beraza <aritz.beraza@gmail.com>
2020-05-29 14:24:58 +0200

    If available use blurr filter as bg

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,11 +1,12 @@
 import { h } from 'preact';
 import { Link } from 'preact-router/match';
 
-// import style from './header.style.scss';
+import style from './header.style.scss';
 import UserWidgetConsumer from './UserWidget.component';
 
+// Style note: blurry replaces bg-black-80
 const Header = () => (
-	<header class="sans-serif bg-black-80 fixed w-100 z-999">
+	<header class={style.blurry + " sans-serif fixed w-100 z-999"}>
 		<nav class="dt w-100 mw8 center">
 			<div class="dtc w2 v-mid pa3">
 				<a href="/" class="div pa1 f3 fw4 tracked white-90 grow-larger no-underline">@Beraza</a>
